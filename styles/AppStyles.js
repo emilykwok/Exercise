60763627ad9b2752707878d5ec0bb0ae51d2f0a1
emilykwok <ekwokchoon@gmail.com>
2019-10-09 10:24:50 -0700

    The fear is real

---
+++ styles/AppStyles.js
@@ -0,0 +1,23 @@
+import {StyleSheet} from 'react-native';
+
+var styles = {
+  start:{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center"
+  }
+  //
+  // contacts:{
+  //   flex:1,
+  //   justifyContent:"center",
+  //   alignItems:"center"
+  // };
+  //
+  // about:{
+  //   flex:1,
+  //   justifyContent:"center",
+  //   alignItems:"center"
+  // };
+};
+
+export default styles;
